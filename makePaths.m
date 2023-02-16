69442d88a40ffbee89a90feba8856623075f6c29
frankenscope <hendricksw@gmail.com>
2023-02-16 09:00:37 -0800

    Update makePaths.m
    change for new fold structure

--- makePaths.m
+++ makePaths.m
@@ -1,16 +1,20 @@
 function makePaths()
 
-addpath(genpath('C:\Users\Holography\Documents\MATLAB\msocket\'));
-rmpath(genpath('C:\Users\Holography\Documents\GitHub\SLM-Managment\'));
-% addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\New_SLM_Code\'));
-rmpath(genpath('C:\Users\Holography\Desktop\SLM_Management\New_SLM_Code\'));
-addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\NOVOCGH_Code\'));
-addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\Calib_Data\'));
-addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\IanTestCode\'));
-addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\MSSocket_SLM\'));
-addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\Basler\'));
-
-addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\New_SLM_Code\SLM_MATLAB\'));
+% addpath(genpath('C:\Users\Holography\Documents\MATLAB\msocket\'));
+% rmpath(genpath('C:\Users\Holography\Documents\GitHub\SLM-Managment\'));
+% % addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\New_SLM_Code\'));
+% rmpath(genpath('C:\Users\Holography\Desktop\SLM_Management\New_SLM_Code\'));
+% addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\NOVOCGH_Code\'));
+% addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\Calib_Data\'));
+% addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\IanTestCode\'));
+% addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\MSSocket_SLM\'));
+% addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\Basler\'));
+% 
+% addpath(genpath('C:\Users\Holography\Desktop\SLM_Management\New_SLM_Code\SLM_MATLAB\'));
+% addpath(genpath('C:\Users\Holography\Desktop\meadowlark'));
+% 
+% disp('done pathing!')
+rt = 'C:\Users\Holography\Desktop\holography\';
+cd(rt)
+addpath(genpath('C:\Users\Holography\Desktop\holography'))
 addpath(genpath('C:\Users\Holography\Desktop\meadowlark'));
-
-disp('done pathing!')
